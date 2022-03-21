ef14115bc32b199dff6e7b6b0db4d9a750cd6da0
Robert Walker <robertWalker6580@gmail.com>
2022-03-21 00:47:50 -0700

    Added the old class version as a comment at the end of the App.js file

--- src/App.js
+++ src/App.js
@@ -44,3 +44,52 @@ const App = () => {
 };
 
 export default App;
+
+// Old class component version
+
+// import { Component } from "react";
+//
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+//
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLowerCase().includes(searchField);
+//     });
+//
+//     return (
+//       <div className="app">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox
+//           placeholder="search monsters"
+//           onChangeHandler={onSearchChange}
+//           className="monsters-search-box"
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
